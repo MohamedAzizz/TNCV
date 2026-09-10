2fbc3531211fc04369b5f047ab2bee5ff2f4ab09
cherniaziz071 <cherniaziz071@gmail.com>
2026-09-10 15:04:30 +0100

    CvServiceApplication.java

---
+++ tncv-backend/cv-service/src/main/java/com/tncv/cv_service/CvServiceApplication.java
@@ -0,0 +1,12 @@
+package com.tncv.cv_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CvServiceApplication.class, args);
+    }
+}
